0b5614c9b3ef1817e7f33b89e5562bef65ec5143
Raphael Sivera <siverar@eleves.enpc.fr>
2013-01-24 00:39:23 +0100

    Réécriture. Meilleur gestion mémoire, date stockée à la seconde (temps UNIX)

--- dhcp/traitement_dhcp_leases.py
+++ dhcp/traitement_dhcp_leases.py
@@ -5,96 +5,122 @@ import netaddr
 import datetime
 import sqlite3
 import sys
+import subprocess
+import time
 
-ORIGINE=datetime.date(1970,1,1)
 
+def str2datetime(d,t):
+    #convertit 2009/12/16,13:38:22 en datetime.date(2009,12,16,13,38,22)
+    if d=="never":
+        return datetime.datetime(2100,1,1)
+    d=d.split('/')
+    t=t.split(':')
+    try:
+        return datetime.datetime(int(d[0]),int(d[1]),int(d[2]),int(t[0]),int(t[1]),int(t[2]))
+    except:
+        print "Probleme conversion des dates : ", s+t
+        return datetime.datetime(1900,1,1)
+def datetime2time(dt):
+    return time.mktime(dt.timetuple())
+
+ORIGINE=datetime2time(datetime.datetime(1970,1,1))
+NOW=datetime2time(datetime.datetime.now())
 
 class Lease:
-    def __init__(self,ip,mac,start,end,hostname=""):
+    def __init__(self,ip,mac,startd,startt,endd,endt,hostname=""):
         self.ip=netaddr.IPAddress(ip)
         self.mac=netaddr.EUI(mac)
-        self.start=str2date(start)
-        self.end=str2date(end)#attention peut contenir "never"
+        self.start=datetime2time(str2datetime(startd,startt))
+        self.end=datetime2time(str2datetime(endd,endt))
         self.hostname=hostname
     def __str__(self):
         return str(self.ip)+" <-> "+str(self.mac)
+    def estrecent(self,ini,fin):
+        return ((self.start>ini)&(self.end>fin))
+    def insert_lease(self,cursor,ini=ORIGINE,fin=NOW):
+    #on ajoute la lease à la bdd ssi elle est récente ie encore valide et pas déjà enregistrée
+        if self.estrecent(ini,fin):
+            cursor.execute('INSERT INTO leases VALUES (?,?,?,?,?)',(int(self.ip),int(self.mac),self.start,self.end,self.hostname))
+            print>>sys.stderr, str(self) 
 
-def str2date(s):
-    #convertit 2009/12/16 en datetime.date(2009,12,16)
-    if s=="never":
-        return "never"
-    s=s.split('/')
-    try:
-        return datetime.date(int(s[0]),int(s[1]),int(s[2]))
-    except:
-        print "Probleme conversion des dates : ", s
-        return datetime.date(1900,1,1)
 
-def lit(nomfichier):
-    liste=[];
+
+def init_base(nombdd):
+    # Si la bdd n'existe pas, la crée
+    '''La BDD contient la table Lease qui contient les ip/mac/time_start/time_end/hostname
+    les dates se comptent en systeme UNIX, 'never'->2100, probleme->1900
+    hostname peut être vide'''
+    
+    test=subprocess.call(["ls",nombdd])
+    if test==0:
+        print "la base existe deja"
+        return 0
+    else:
+        conn=sqlite3.connect(nombdd)
+        try:
+            conn.execute('CREATE TABLE leases(ip INT,mac INT,date_start INT,date_end INT,hostname VARCHAR(20))')
+            conn.commit()
+            conn.close()
+            print "la base a ete creee" 
+            return 0
+        except:
+            print "probleme lors de la creation de la base de donnees"
+            conn.close()
+            return 1
+        
+def lit(nomfichier,nombdd):
+    '''lit le fichier et stocke au fur et à mesure les informations dans la bdd. On ne stocke que les info plus récentes que début et tjrs d'actualité'''
+    l=[]#ligne du fichier
+
+    #initialisation de la bdd - debut : date de dernière modif
+    init_base(nombdd)
+    conn=sqlite3.connect(nombdd)
+    curs=conn.cursor()
+    curs.execute('SELECT max(date_start) FROM leases')
+    debut=curs.fetchall()
+    debut=debut[0][0]
+    if debut==None:
+        debut=ORIGINE
+
     #on ouvre le fichier en lecture
+    # et on lit les lignes 1 à 1
     with open(nomfichier,'r') as f:
-        f=f.readlines()
-        L=len(f)#nbre de lignes
-        nl=0
-        #boucle sur les lignes
-        while nl<L:
-            l=f[nl].strip('\n ;').split(' ')
-            nl+=1
+        for ligne in f.readlines():
+            l=ligne.strip('\n ;').split(' ')
+            # si on débute une lease, on prépare le stockage des infos
             if l[0] == "lease":
                 ip=l[1]
+                # on reinitialise les données cherchées
                 mac=""
-                start=""
-                end=""
+                startd=""
+                startt=""
+                endd=""
+                endt=""
                 host=""
-                while l[0] != "}" and nl<L:
-                    l=f[nl].strip('\n ;').split(' ')
-                    nl+=1
-                    if l[0]=="starts":
-                        start=l[2]
-                    if l[0]=="ends":
-                        if l[1]=="never":
-                            end="never"
-                        else:
-                            end=l[2]
-                    if l[0]=="hardware":
-                        mac=l[2]
-                    if l[0]=="client-hostname":
-                        host=l[1]
-                liste.append(Lease(ip,mac,start,end,host))
-        return liste
+            # si on a fini une lease on stocke
+            elif l[0] == "}":
+                L=Lease(ip,mac,startd,startt,endd,endt,host)
+                L.insert_lease(curs,debut)# verifie les dates
+            #sinon on cherche des données
+            elif l[0] == "lease":
+                ip=l[1]
+            elif l[0]=="starts":
+                startd=l[2]
+                startt=l[3]
+            elif l[0]=="ends":
+                if l[1]=="never":
+                    endd="never"
+                else:
+                    endd=l[2]
+                    endt=l[3]
+            elif l[0]=="hardware":
+                mac=l[2]
+            elif l[0]=="client-hostname":
+                host=l[1]
 
-def stock(liste,path):
-    debut=(datetime.date(2012,1,1)-ORIGINE).days
-    connexion=sqlite3.connect(path)
-    '''La BDD contient la table usr contient les ip/mac/date_start/date_end/hostname
-    les dates se comptent en jours depuis le 1970/1/1, -1 signifie 'never'
-    hostname peut être vide'''
-    #on teste si la base existe déjà, le cas écheant on remplie uniquement avec les données récentes
-    try:
-        connexion.execute('CREATE TABLE usr(ip INT,mac INT,date_start INT,date_end INT,hostname VARCHAR(20))')
-        connexion.commit()
-        print "creation de la base" 
-    except:
-        print "la base existe déjà"
-        curseur=connexion.cursor()
-        curseur.execute('SELECT max(date_start) as delta FROM usr')
-        debut=curseur.fetchall()
-        debut=debut[0][0]
-    for lease in liste:
-        n=0
-        if (lease.end=='never'):
-            n=-1
-        elif (lease.end>=datetime.date.today() and (lease.start-ORIGINE).days>=debut):
-            n=(lease.end-ORIGINE).days
-        if n!=0:
-            connexion.execute('INSERT INTO usr VALUES (?,?,?,?,?)',(int(lease.ip),int(lease.mac),(lease.start-ORIGINE).days,n,lease.hostname))
-        connexion.commit()
-        print>>sys.stderr, lease.start-ORIGINE
-        print>>sys.stderr, lease
-        print>>sys.stderr, lease.end
-    connexion.close()
+    conn.commit()
+    conn.close()
+
 
 if __name__=='__main__':
-    l=lit("logdhcp")
-    stock(l,'./utilisateurs.sqlite3')
+    lit("logdhcp","leases.db")
