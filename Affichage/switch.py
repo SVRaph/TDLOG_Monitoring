b7807e868509c40cf00cd29c6ca126231801fbcd
SvRaph <siverar@eleves.enpc.fr>
2013-02-17 19:52:44 +0100

    Menage (part. 2)

--- Affichage/switch.py
+++ Affichage/switch.py
@@ -7,7 +7,8 @@
 #Ajouter de la robustesse => vérifier qu'on ajoute pas deux fois le même switch avec la même IP
 class Switch:
     # Constructeur
-    def __init__(self,IP,mac):
+    def __init__(self,IP,mac,nom):
+        self.nom = nom
         self.IP = IP
         self.mac = mac
         self.ping = "0" # 0 si le switch répond à ping IP
@@ -26,9 +27,9 @@ class Switch:
     # Affichage HTML de l'arboresscence des switch (à partir de celui-ci)
     def afficheHTML(self, prefix, prefixB):
         if self.ping=="0":
-            code = prefix + "IP: " + str(self.IP) + " (MAC: "+ self.mac + ")<br>"
+            code = prefix + "<i>" + self.nom + "</i>, IP: " + str(self.IP) + " (MAC: "+ self.mac + ")<br>"
         else:
-            code = prefix + '<font color="red"><b>IP: ' + str(self.IP) + "</b></font> (MAC: "+ self.mac + ")<br>"
+            code = prefix + '<font color="red"><b><i>' + self.nom + '</i>, IP: ' + str(self.IP) + "</b></font> (MAC: "+ self.mac + ")<br>"
         taille = len(self.fils)
         if taille>0:
             code += prefixB + "|<br>"
@@ -54,56 +55,59 @@ class Switch:
 #------------------------------------------------------------
 
 # Routeur
-routeur = Switch("172.24.0.1","inconnu")
-
-# Rezo
-routeur.fils.append(Switch("172.24.0.10", "inconnu"))
+routeur = Switch("172.24.0.1","inconnu","Routeur")
 
 # Cyber
-routeur.fils.append(Switch("172.24.0.11", "inconnu"))
+routeur.fils.append(Switch("172.24.0.3", "inconnu","Cyber"))
+
+# Rezo
+routeur.fils.append(Switch("172.24.0.10", "inconnu","Rezo"))
 
 # Berbere
-routeur.fils.append(Switch("172.24.0.20", "inconnu"))
+routeur.fils.append(Switch("172.24.0.20", "inconnu","Berbere"))
 
 # Touareg
-routeur.fils.append(Switch("172.24.0.21", "inconnu"))
+routeur.fils.append(Switch("172.24.0.21", "inconnu","Touareg"))
 
 # Bedouin
-routeur.fils.append(Switch("172.24.0.22", "inconnu"))
+routeur.fils.append(Switch("172.24.0.22", "inconnu","Bedouin"))
 
 # Colossus
-routeur.fils.append(Switch("172.24.0.23", "inconnu"))
+routeur.fils.append(Switch("172.24.0.23", "inconnu","Colossus"))
+
+# Clubinfo
+routeur.fils.append(Switch("195.221.194.9","inconnu","Clubinfo"))
 
 # Local technique
-switchMeunier = Switch("172.24.0.101","inconnu")
+switchMeunier = Switch("172.24.0.101","inconnu","Local technique (fibre)")
 routeur.fils.append(switchMeunier)
 
 # Local technique -> Meunier
-switchMeunier.fils.append(Switch("172.24.0.103","inconnu"))
+switchMeunier.fils.append(Switch("172.24.0.103","inconnu","Local technique"))
 
 # Armoire 1 de Meunier
-switchMeunier.fils[0].fils.append(Switch("172.24.0.111","inconnu"))
-switchMeunier.fils[0].fils.append(Switch("172.24.0.112","00:18:4d:d7:5b:17"))
-switchMeunier.fils[0].fils.append(Switch("172.24.0.113","00:18:4d:d7:5b:1d"))
-switchMeunier.fils[0].fils.append(Switch("172.24.0.114","74:44:01:81:93:90"))
+switchMeunier.fils[0].fils.append(Switch("172.24.0.111","inconnu","Armoire 1"))
+switchMeunier.fils[0].fils.append(Switch("172.24.0.112","00:18:4d:d7:5b:17","Armoire 1"))
+switchMeunier.fils[0].fils.append(Switch("172.24.0.113","00:18:4d:d7:5b:1d","Armoire 1"))
+switchMeunier.fils[0].fils.append(Switch("172.24.0.114","74:44:01:81:93:90","Armoire 1"))
 
 # Armoire 2 de Meunier
-switchMeunier.fils[0].fils.append(Switch("172.24.0.121","20:4e:7f:8b:67:02"))
-switchMeunier.fils[0].fils.append(Switch("172.24.0.122","00:18:4d:d5:46:52"))
+switchMeunier.fils[0].fils.append(Switch("172.24.0.121","20:4e:7f:8b:67:02","Armoire 2"))
+switchMeunier.fils[0].fils.append(Switch("172.24.0.122","00:18:4d:d5:46:52","Armoire 2"))
 
 # Armoire 3 de Meunier
-switchMeunier.fils[0].fils.append(Switch("172.24.0.131","74:44:01:81:93:95"))
-switchMeunier.fils[0].fils.append(Switch("172.24.0.132","74:44:01:81:93:97"))
-switchMeunier.fils[0].fils.append(Switch("172.24.0.133","00:1e:2a:db:dc:3c"))
+switchMeunier.fils[0].fils.append(Switch("172.24.0.131","74:44:01:81:93:95","Armoire 3"))
+switchMeunier.fils[0].fils.append(Switch("172.24.0.132","74:44:01:81:93:97","Armoire 3"))
+switchMeunier.fils[0].fils.append(Switch("172.24.0.133","00:1e:2a:db:dc:3c","Armoire 3"))
 
 # Armoire 4 de Meunier
-switchMeunier.fils[0].fils.append(Switch("inconnu","00:1B:2F:B0:15:54"))
-switchMeunier.fils[0].fils.append(Switch("172.24.0.142","000:18:4d:d4:35:e4"))
-switchMeunier.fils[0].fils.append(Switch("172.24.0.143","00:1E:2A:DB:BD:70"))
-switchMeunier.fils[0].fils.append(Switch("172.24.0.144","00:18:4d:2e:89:54"))
+switchMeunier.fils[0].fils.append(Switch("inconnu","00:1B:2F:B0:15:54","Armoire 4"))
+switchMeunier.fils[0].fils.append(Switch("172.24.0.142","000:18:4d:d4:35:e4","Armoire 4"))
+switchMeunier.fils[0].fils.append(Switch("172.24.0.143","00:1E:2A:DB:BD:70","Armoire 4"))
+switchMeunier.fils[0].fils.append(Switch("172.24.0.144","00:18:4d:2e:89:54","Armoire 4"))
 
 # local technique -> Perronet
-switchMeunier.fils.append(Switch("172.24.0.100","00:0a:57:12:cd:e1"))
-switchMeunier.fils.append(Switch("172.24.0.104","00:0a:57:dc:2b:21"))
-switchMeunier.fils.append(Switch("172.24.0.203","20:4e:7f:8b:67:35"))
-switchMeunier.fils.append(Switch("172.24.0.204","20:4e:7f:8b:67:1e"))
+switchMeunier.fils.append(Switch("172.24.0.100","00:0a:57:12:cd:e1","Perronet 1"))
+switchMeunier.fils.append(Switch("172.24.0.104","00:0a:57:dc:2b:21","Perronet 2"))
+switchMeunier.fils.append(Switch("172.24.0.203","20:4e:7f:8b:67:35","Perronet 3"))
+switchMeunier.fils.append(Switch("172.24.0.204","20:4e:7f:8b:67:1e","Perronet 4"))
